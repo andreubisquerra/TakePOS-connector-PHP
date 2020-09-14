93d70195c5a25b9abad23a9477e763ac44a1c640
javierybar <javierybar@outlook.es>
2020-09-14 09:49:34 +0200

    Fix scale decimals

--- scale/Scale.java
+++ scale/Scale.java
@@ -6,11 +6,11 @@
 package scale;
 
 import static java.lang.System.out;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jssc.SerialPort;
 import jssc.SerialPortException;
-import jssc.SerialPortList;
 
 /**
  *
@@ -34,8 +34,9 @@ public class Scale {
             serialPort.openPort();
             serialPort.setParams(9600, 8, 1, 0);
             serialPort.writeBytes("$".getBytes());
-            byte[] buffer = serialPort.readBytes(1);
-            out.println(serialPort.readString());
+            byte buffer[] = serialPort.readBytes(10);
+            String s = new String(buffer, StandardCharsets.UTF_8);
+            out.println(s);
             serialPort.closePort();
         } catch (SerialPortException ex) {
             Logger.getLogger(Scale.class.getName()).log(Level.SEVERE, null, ex);

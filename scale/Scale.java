50e486b4ea06966e637e6d10e74f64ccae1b9d87
andreubisquerra <jove@bisquerra.com>
2021-06-28 15:04:42 +0200

    Scale BIG improvement

--- scale/Scale.java
+++ scale/Scale.java
@@ -5,12 +5,11 @@
  */
 package scale;
 
-import static java.lang.System.out;
-import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jssc.SerialPort;
 import jssc.SerialPortException;
+import jssc.SerialPortList;
 
 /**
  *
@@ -20,31 +19,50 @@ public class Scale {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
-    public static void main(String[] args) {
-        
-        
-        
-        
-       SerialPort serialPort = new SerialPort("COM1");
+    public static void main(String[] args) throws Exception {
 
-              
-       try {
-            
-            serialPort.openPort();
-            serialPort.setParams(9600, 8, 1, 0);
+        String[] portNames = SerialPortList.getPortNames();
+        SerialPort serialPort = null;
+        for (String name : portNames) {
+            //System.out.println("Port : " + name);
+            if (name.equals("COM1")) {
+                serialPort = new SerialPort(name);
+                break;
+            }
+        }
+
+        if (serialPort == null) {
+            throw new IllegalStateException("COM 1 port not found");
+        }
+
+        try {
+            if (!serialPort.isOpened()) {
+                serialPort.openPort();
+            }
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8, SerialPort.STOPBITS_2, SerialPort.PARITY_NONE);
+
             serialPort.writeBytes("$".getBytes());
-            byte buffer[] = serialPort.readBytes(10);
-            String s = new String(buffer, StandardCharsets.UTF_8);
-            out.println(s);
-            serialPort.closePort();
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < serialPort.getLinesStatus().length; i++) {
+                builder.append(serialPort.readString(SerialPort.STOPBITS_2));
+            }
+            System.out.println(builder.toString());
+
         } catch (SerialPortException ex) {
             Logger.getLogger(Scale.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (serialPort.isOpened()) {
+                serialPort.closePort();
+                //System.err.println("Serial Prot Closed!");
+            } else {
+                //System.err.println("Serial Prot Already Closed!");
+            }
         }
-                   
-        
-        
-        
+
     }
-    
+
 }

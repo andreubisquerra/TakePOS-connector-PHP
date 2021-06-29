516344fef0895b3694ef64a3e2c429dede1ef77b
jove@bisquerra.com <jove@bisquerra.com>
2021-06-29 19:11:25 +0200

    Kill java process if serial port is busy

--- scale/index.php
+++ scale/index.php
@@ -3,8 +3,10 @@ header('Access-Control-Allow-Origin: *');
 $arg1 = "Multi Return";
 exec("java -jar scale.jar $arg1",$output);
 
+if (empty($output)){
+	exec("taskkill /F /IM java.exe ");
+	exec("java -jar scale.jar $arg1",$output);
+}
+
 echo preg_replace("/[^0-9.]/", "", $output[0]);;
-
-
-
 ?>
